ee58776893d348ecf88b8e9a1f245a30b2214fb6
FabienHP <fabienfiette@hotmail.fr>
2019-09-26 15:02:28 +0200

    remise a zero avant js

--- Wild-Code-School/01 js/hello-wilder.js
+++
@@ -1,6 +0,0 @@
-const movieName = "Forrest Gump,";
-const date = 1994;
-const actName = " réalisé par Robert Zemeckis,"
-const message = movieName + actName + " est sorti en " + date + ".";
-
-alert(message);
