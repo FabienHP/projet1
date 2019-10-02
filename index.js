dad0543e68445fc79a33516fd8a91cab804ab621
FabienHP <fabienfiette@hotmail.fr>
2019-10-02 18:01:20 +0200

    update 18h mer

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
 const submit = document.getElementById("button-form");
 
 submit.addEventListener("click", () => {
-    let firstName1 = document.getElementById("First-name").value;
-    let lastName1 = document.getElementById("Last-name").value;
-    let adress1 = document.getElementById("Email-adresse").value;
+    let firstName1 = document.getElementById("firstName").value;
+    let lastName1 = document.getElementById("lastName").value;
+    let adress1 = document.getElementById("emailAddress").value;
+    let password1 = document.getElementById("password").value;
+    let formDisplay = document.getElementById("form");
+    let welcome = document.getElementById("welcome");
 
     const formcont = {
         firstName: firstName1,
         lastName: lastName1,
-        adress: adress1
+        adress: adress1,
+        password: password1
     };
 
-    switch (firstName1) {
-        case '':
-            alert(`Enter your first name`);
-            break;
-        default:
-            switch (lastName1) {
-                case '':
-                    alert(`Enter your last name`);
-                    break;
-                default:
-                    switch (adress1) {
-                        case '':
-                            alert(`Enter your email adress`);
-                            break;
-                        default:
-                            console.log(`${Object.keys(formcont)[0]}:`, firstName1);
-                            console.log(`${Object.keys(formcont)[1]}:`, lastName1);
-                            console.log(`${Object.keys(formcont)[2]}:`, adress1);
-                            break;
-                        };        
-                };            
-            };
+    if (firstName1 === '') {
+        alert(`Enter your first name`);
+    } else if (lastName1 === '') {
+        alert(`Enter your last name`);
+    } else if (adress1 === '') {
+        alert(`Enter your email address`);
+    } else if (password1 === '') {
+        alert(`Enter your password`);
+    } else {
+        console.log(`${Object.keys(formcont)[0]}:`, firstName1);
+        console.log(`${Object.keys(formcont)[1]}:`, lastName1);
+        console.log(`${Object.keys(formcont)[2]}:`, adress1);
+        formDisplay.style.display = 'none';
+        welcome.style.display = 'block';
+    }
 });
--- redirection.js
+++
@@ -1,5 +0,0 @@
-const back = document.getElementById("button-back");
-
-back.addEventListener("click", () => {
-    window.location='file:///home/fabien/Documents/projet1/index.html'
-})
